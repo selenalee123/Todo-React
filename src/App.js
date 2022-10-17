22ff43fdf18d739e019d4b107e40e10ceb451980
Kim Kim <52686208+selenalee123@users.noreply.github.com>
2022-10-16 21:22:39 -0400

    add commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./App.css";
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormTodo from "./components/FormTodo";
+import Todo from "./components/Todo";
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "This is a sampe todo",
+      isDone: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+return (
+  <div className="app">
+    <div className="container">
+      <h1 className="text-center mb-4">Todo List</h1>
+      <FormTodo addTodo={addTodo} />
+
+      {todos.map((todo, index) => (
+        <Card key={index} className="mb-4">
+          <Card.Body>
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              markTodo={markTodo}
+              removeTodo={removeTodo}
+            />
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  </div>
+);
+}
+
+export default App;
